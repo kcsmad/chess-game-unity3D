732141bdbd15466445d3370629637841d5d70ef1
Kevin Maduro <nivekzin@gmail.com>
2019-04-21 17:08:01 -0300

    basics: Created GameManager and the BoardManager for handling board generation

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+
+    private const float TILE_SIZE = 1.0f;
+    private const float TILE_OFFSET = 0.5f;
+
+    private int selectionX = -1;
+    private int selectionY = -1;
+
+    public List<GameObject> chessmanPrefabs;
+    private List<GameObject> activeChessman = new List<GameObject>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnChessman(0, new Vector3(1.5f, 0.5f, 0.5f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateSelection();
+        DrawChessBoard();
+    }
+
+    private void UpdateSelection()
+    {
+        if (!Camera.main)
+        {
+            return;
+        }
+
+        RaycastHit hit;
+        if (Physics.Raycast(
+            Camera.main.ScreenPointToRay(Input.mousePosition), 
+            out hit, 
+            25.0f, 
+            LayerMask.GetMask("ChessPlane"))
+        )
+        {
+            selectionX = (int) hit.point.x;
+            selectionY = (int) hit.point.z;
+        }
+        else
+        {
+            selectionX = -1;
+            selectionY = -1;
+        }
+        
+    }
+
+    private void SpawnChessman(int index, Vector3 position)
+    {
+        GameObject go = Instantiate(chessmanPrefabs[index], position, Quaternion.identity) as GameObject;
+        go.transform.SetParent(transform);
+        activeChessman.Add(go);
+    }
+
+    private void DrawChessBoard()
+    {
+        Vector3 widthLine = Vector3.right * 8;
+        Vector3 heightLine = Vector3.forward * 8;
+
+        for (int i = 0; i <= 8; i++)
+        {
+            Vector3 start = Vector3.forward * i;
+            Debug.DrawLine(start, start + widthLine);
+            
+            for (int j = 0; j <= 8; j++)
+            {
+                start = Vector3.right * j;
+                Debug.DrawLine(start, start + heightLine);
+            }            
+        }
+
+        if (selectionX >= 0 && selectionY >= 0)
+        {
+            Debug.DrawLine(
+                Vector3.forward * selectionY + Vector3.right * selectionX,
+                Vector3.forward * (selectionY + 1) + Vector3.right * (selectionX + 1)
+            );
+            
+            Debug.DrawLine(
+                Vector3.forward * (selectionY + 1) + Vector3.right * selectionX,
+                Vector3.forward * selectionY + Vector3.right * (selectionX + 1)
+            );
+        }
+    }
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    private const float TILE_SIZE = 1.0f;
+    private const float TILE_OFFSET = 0.5f;
+
+    private int selectionX = -1;
+    private int selectionY = -1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +21,22 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        DrawChessBoard();
+    }
+
+    private void DrawChessBoard()
+    {
+        Vector3 widthLine = Vector3.right * 8;
+        Vector3 heightLine = Vector3.forward * 8;
+
+        for (int i = 0; i <= 8; i++)
+        {
+            Vector3 start = Vector3.forward * i;
+            Debug.DrawLine(start, start + widthLine);
+            for (int j = 0; j <= 8; j++)
+            {
+                
+            }            
+        }
     }
 }
